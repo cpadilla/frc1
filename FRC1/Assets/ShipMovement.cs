607ed66e3a1b961d8f5ed8ba27dad5e19bae1288
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-18 20:20:36 -0400

    adding ship assets

---
+++ FRC1/Assets/ShipMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Sprites;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    }
+
+
+}
