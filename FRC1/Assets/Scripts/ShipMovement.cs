5158a1f3a75e4d28aad6370baf725de19e14a120
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-18 20:51:05 -0400

    Commiting

---
+++ FRC1/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour
+{
+	//Variables
+	public float m_speed=1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*m_speed, Input.GetAxis("Vertical")*Time.deltaTime*m_speed, 0);
+    }
+
+
+}
