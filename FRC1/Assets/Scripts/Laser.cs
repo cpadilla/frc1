6bf46575ee2bff7a1484bdd5223020e4957825ca
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-19 01:04:21 -0400

    Committing

--- FRC1/Assets/Scripts/Laser.cs
+++ FRC1/Assets/Scripts/Laser.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class Laser : MonoBehaviour {
 
+	public float m_lifeTime=3.0f;
 
 	public Vector2 velocity;
 
 	// Use this for initialization
 	void Start () {
-
+		Destroy( gameObject, m_lifeTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 
 		//this.transform.position += this.transform.forward*Time.deltaTime;
 
