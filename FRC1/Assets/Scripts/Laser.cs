f40fb4f49fe7aeb3f201d04e54ecf14970a41743
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-19 00:42:25 -0400

    Shooting and Laser

--- FRC1/Assets/Scripts/Laser.cs
+++ FRC1/Assets/Scripts/Laser.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class Laser : MonoBehaviour {
 
 
+	public Vector2 velocity;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		//this.transform.position += this.transform.forward*Time.deltaTime;
+
+		transform.position += transform.up * Time.deltaTime*100;
 	}
 }
--- FRC1/Assets/Scripts/PlayerShoot.cs
+++ FRC1/Assets/Scripts/PlayerShoot.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+
 using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
@@ -6,7 +7,15 @@ public class PlayerShoot : MonoBehaviour {
 	//Bullet to shoot
 	public GameObject bullet;
 
+	//Velocity?
 	public Vector2 velocity;
+
+
+	//Fire rate
+	public float m_fireRate=0.5f;
+	float m_timer=0.0f;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +26,23 @@ public class PlayerShoot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetButton("Fire1"))
+		m_timer += Time.deltaTime;
+
+		if(Input.GetButton("Fire1") && m_timer >= m_fireRate )
 		{
 
 			Vector3 pos= this.transform.position;
 
-			GameObject nBullet= (GameObject)Instantiate(bullet,pos,new Quaternion(0,0,0,0));
 
+			GameObject nBullet= (GameObject)Instantiate(bullet,(transform.position),transform.rotation);
+
+
+			nBullet.transform.Translate(new Vector3(0,0,1));
+
+
+			//nBullet.transform.Rotate(new Vector3(0,90,0));
+
+			m_timer=0.0f;
 
 		}
 	}
