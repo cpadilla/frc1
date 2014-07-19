13657fe8cdb05aef9721a753f4c37d895c599833
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-18 23:08:33 -0400

    Adding

---
+++ FRC1/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	//Bullet to shoot
+	public GameObject bullet;
+
+	public Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetButton("Fire1"))
+		{
+
+			Vector3 pos= this.transform.position;
+
+			GameObject nBullet= (GameObject)Instantiate(bullet,pos,new Quaternion(0,0,0,0));
+
+
+		}
+	}
+
+	//Input
+}
