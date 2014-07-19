f87670e43a90adb8996077b39015e215ce896b35
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-19 01:26:41 -0400

    Good build

--- FRC1/Assets/Scripts/PlayerShoot.cs
+++ FRC1/Assets/Scripts/PlayerShoot.cs
@@ -4,23 +4,22 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
+	//
 	//Bullet to shoot
 	public GameObject bullet;
 
 	//Velocity?
 	public Vector2 velocity;
-
-
+	
 	//Fire rate
 	public float m_fireRate=0.5f;
 	float m_timer=0.0f;
 
-
+	//GameObject player= GetComponent<Player>;
 
 	// Use this for initialization
 	void Start () {
 	
-
 	}
 	
 	// Update is called once per frame
@@ -30,6 +29,7 @@ public class PlayerShoot : MonoBehaviour {
 
 		if(Input.GetButton("Fire1") && m_timer >= m_fireRate )
 		{
+
 
 			Vector3 pos= this.transform.position;
 
