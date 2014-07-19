01bed08533b8cab4d25ee5e713dab19bbc5b192f
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-19 17:16:09 -0400

    Adding what I have for lights

---
+++ FRC1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+        public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            transform.rotation = player.transform.rotation;
+            transform.position = player.transform.position;
+            transform.Rotate(-90, 0, 0);
+	}
+}
