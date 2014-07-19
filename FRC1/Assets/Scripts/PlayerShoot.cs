f07a6c102ffa5adc6805ab92da4c912bf0fea7c7
gamingod4u <mcman407@gmail.com>
2014-07-19 14:50:29 -0400

    Working on shooting

--- FRC1/Assets/Scripts/PlayerShoot.cs
+++ FRC1/Assets/Scripts/PlayerShoot.cs
@@ -4,57 +4,100 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
-	//
-	//Bullet to shoot
-	public GameObject bullet;
-
-	//Velocity?
-	public Vector2 velocity;
 	
-	//Fire rate
-	public float m_fireRate	=	0.000001f;
-	float m_timer=0.0f;
+	public GameObject [] 	bulletTypes;
+	public GameObject [] 	mainGuns;
+	public GameObject [] 	sideGuns; 
+	
+	[HideInInspector]
+	public float 			m_fireRate	=	1f;
+	
+	private float [] 		m_fireRateMult;
+	private int				m_fireRateIndex = 0; 	
+	private float 			m_Timer=0.0f;
 
 	//GameObject player= GetComponent<Player>;
-	public GameObject[]  mainGuns;
+
 	
 	private GameObject   nBullet;
-	private bool		 RightGun = false;
+	private bool		 mainGun_stageredFire = true;
+	private bool 		 mainGun_doubleFire = false;
+	private bool		 mainGun_stageredFireRight = true;
 	
+	private bool		 doubleGuns_stagaredFire = false;
+	private bool 		 doubleGuns_doubleFire = false;
+     
+	private float 		 m_fBulletSpeed = 1.0f;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		m_fireRateMult = new float[3];
+		m_fireRateMult[0] = 2.5f;
+		m_fireRateMult[1] = 5.0f;
+		m_fireRateMult[2] = 7.5;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
-		if(Input.GetButton("Fire1") && Time.time >= Time.deltaTime + m_fireRate )
+		m_Timer += Time.deltaTime * m_fireRateMult[m_fireRateIndex];
+		
+		if(Input.GetMouseButton(0) && m_Timer >=  m_fireRate)
 		{
-			Fire ();		
+			Fire ();
+			m_Timer = 0;
 		}
+		Testing();
+		
+		
 	}
 
 	private void Fire()
 	{
-		if(RightGun)
-		{		
-			nBullet= (GameObject)Instantiate(bullet,(mainGuns[1].transform.position),transform.rotation);
-			RightGun = false;
-		}
-		else
+		if(mainGun_stageredFire)
 		{
-			nBullet= (GameObject)Instantiate(bullet,(mainGuns[0].transform.position),transform.rotation);
-			RightGun = true;
+			if(mainGun_stageredFireRight)
+			{		
+				nBullet= (GameObject)Instantiate(bulletTypes[0],(mainGuns[1].transform.position),transform.rotation);
+				mainGun_stageredFireRight = false;
+			}
+			else
+			{
+				nBullet= (GameObject)Instantiate(bulletTypes[0],(mainGuns[0].transform.position),transform.rotation);
+				mainGun_stageredFireRight = true;
+			}
+			
+			nBullet.transform.Translate(new Vector3(0,0,m_fBulletSpeed));
 		}
-		
-		
-		nBullet.transform.Translate(new Vector3(0,0,1));
-		
-		
-		//nBullet.transform.Rotate(new Vector3(0,90,0));
-		
-		m_timer=0.0f;
+		else if(mainGun_doubleFire)
+		{
+			nBullet= (GameObject)Instantiate(bulletTypes[0],(mainGuns[1].transform.position),transform.rotation);
+			nBullet= (GameObject)Instantiate(bulletTypes[0],(mainGuns[0].transform.position),transform.rotation);
+		}
+			
+	}
+	
+	private void Testing()
+	{
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			if(mainGun_stageredFire)
+			{
+				mainGun_stageredFire = false;
+				mainGun_doubleFire = true;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			m_fireRateIndex  = 0;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			m_fireRateIndex  = 1;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			m_fireRateIndex  = 2;
+		}
 	}
 }
