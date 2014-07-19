efa850de492215c68bb76416893d33f400ce3e52
gamingod4u <mcman407@gmail.com>
2014-07-19 11:32:12 -0400

    Assets for ahip and stuff

---
+++ FRC1/Assets/Scripts/Planet Rotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotation : MonoBehaviour {
+	
+	private GameObject player;
+	
+	
+	void Awake()
+	{
+	  	
+	}
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
