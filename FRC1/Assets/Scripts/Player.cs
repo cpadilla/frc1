12f073ed8ced6a7407903e1b7839cacc2291721f
gamingod4u <mcman407@gmail.com>
2014-07-19 02:54:56 -0400

    Update Player

--- FRC1/Assets/Scripts/Player.cs
+++ FRC1/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 	//Variables
         public float m_speed = 10.00f;
         public float r_speed = 20.0f;
-<<<<<<< HEAD
+
         public bool moving = false;
 		
 		
@@ -17,20 +17,13 @@ public class Player : MonoBehaviour
         void Awake()
         {
         	Instance = this;
-=======
-        public bool moving = false;
-        public static Player instance;
-
+       }
         // Use this for initialization
-        void Start()
-        {
-            instance = this;
->>>>>>> fc9eb9f9aca26343b2946629948b40a229549a94
-        }
+        
 
         // Update is called once per frame
         void Update()
-<<<<<<< HEAD
+
         {
         	// Store the input from the player
             v_Input = Input.GetAxis("Vertical");
@@ -42,12 +35,7 @@ public class Player : MonoBehaviour
             // translate the input read from player this iteration 
             transform.Rotate(0,0, h_Input * Time.deltaTime * r_speed);
             transform.Translate(0, v_Input * Time.deltaTime * m_speed, 0);
-=======
-        {
-            moving = (Mathf.Abs(Input.GetAxis("Vertical")) > 0);
-            transform.Rotate(0,0,-Input.GetAxis("Horizontal") *Time.deltaTime* r_speed);
-            transform.Translate(0, Input.GetAxis("Vertical")*Time.deltaTime*m_speed, 0);
->>>>>>> fc9eb9f9aca26343b2946629948b40a229549a94
+
         }
 
 		//transform.Rotate(Input.GetButton("Up"),90.0f);
