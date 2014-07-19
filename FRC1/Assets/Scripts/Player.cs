85253ed9b243888f66e223834ffbe348a9551caf
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-19 02:09:03 -0400

    added moved bool

--- FRC1/Assets/Scripts/Player.cs
+++ FRC1/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 	//Variables
         public float m_speed = 10.00f;
         public float r_speed = 20.0f;
+        public bool moving = false;
 
         // Use this for initialization
         void Start()
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour
         // Update is called once per frame
         void Update()
         {
+            moving = (Mathf.Abs(Input.GetAxis("Vertical")) > 0);
             transform.Rotate(0,0,-Input.GetAxis("Horizontal") *Time.deltaTime* r_speed);
             transform.Translate(0, Input.GetAxis("Vertical")*Time.deltaTime*m_speed, 0);
         }
