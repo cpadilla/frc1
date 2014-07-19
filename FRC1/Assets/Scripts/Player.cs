55099641734630e7c7d67889993dabd95b82bbaf
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-18 23:48:47 -0400

    removed dumb player static class

--- FRC1/Assets/Scripts/Player.cs
+++ FRC1/Assets/Scripts/Player.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine; using System.Collections; 
+public class Player : MonoBehaviour
+{
+	//Variables
+        public float m_speed = 1.00f;
 
-public static class Player {
+        // Use this for initialization
+        void Start()
+        {
+        }
 
-    //Variables
-    public static float m_speed;
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(0,0,Input.GetAxis("Horizontal") * Time.deltaTime * m_speed);
+            transform.Translate(0, Input.GetAxis("Vertical")*Time.deltaTime*m_speed, 0);
+        }
 
-    public static void Initialize() {
-        m_speed = 1.0f;
+		//transform.Rotate(Input.GetButton("Up"),90.0f);
 
-    }
+	}
 
-}
+
+
