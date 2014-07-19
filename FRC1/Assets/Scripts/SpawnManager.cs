4da6593a74e5b5d06e5ae53ce34b9274a23e9cf8
John Li <johnli@johns-mbp.fullsail.com>
2014-07-19 02:30:07 -0400

    Committing my scene

---
+++ FRC1/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviour {
+
+    public List<GameObject> spawners;
+
+	public float spawnInterval = .5f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (CR_Spawn ());
+	}
+
+	IEnumerator CR_Spawn()
+	{
+		while (true) {
+			Instantiate (spawners [0]);
+			yield return new WaitForSeconds (spawnInterval);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
