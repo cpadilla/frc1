9f9ecb1900896c6287592f4160f3785445ea3b0f
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-19 21:26:07 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	FRC1/Assets/Prefabs/Piece.prefab.meta

---
+++ FRC1/Assets/Scripts/GameplayUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayUI : MonoBehaviour {
+
+	public GUIText[] enemyCounters;
+	public GUIText score;
+
+	private static GameplayUI instance;
+
+	public static GameplayUI getInstance()
+	{
+		if (instance == null)
+			instance = new GameplayUI ();
+
+
+		return instance;
+	}
+
+	public void UpdateScore(int amount)
+	{
+		score.text = amount.ToString ();
+	}
+	public void UpdateEnemyCounter(int Index, int amount)
+	{
+		enemyCounters [Index].text = amount.ToString ();
+	}
+
+}
