72bff15e59476f94b5b524ad9c4344ab028cb6b1
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-19 21:59:09 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	FRC1/Assets/Prefabs/Piece.prefab.meta

---
+++ FRC1/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	
+	public GameObject[] enemy;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
