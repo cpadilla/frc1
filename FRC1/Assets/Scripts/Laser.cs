2baf743118f280f0997132a748b61ded11a2ceed
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-20 04:28:07 -0400

    Failed to implement firing patterns
    Don't mind me I'm just changing stuff

--- FRC1/Assets/Scripts/Laser.cs
+++ FRC1/Assets/Scripts/Laser.cs
@@ -8,6 +8,18 @@ public class Laser :Unit {
 	public Vector2 velocity;
 
 
+        // Factory for laser
+        public static GameObject CreateLaser(string owner, GameObject bulletType, Vector3 postion, Quaternion rotation)
+        {
+            GameObject obj = (GameObject)Instantiate(bulletType,postion,rotation);
+
+            // Try to set the owner with the provided value
+            try { obj.tag = owner; }
+            catch { obj.tag = "Laser"; }
+
+            return obj;
+        }
+
 	// Use this for initialization
 	void Start () {
 		gameObject.name = "Laser";
