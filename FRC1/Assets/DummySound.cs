83327f9cf5da996bd369631b11f66b8846142f20
Christian Benavides <chrisloco11@fullsail.edu>
2014-07-20 17:48:46 -0400

    Sounds and other featrues

---
+++ FRC1/Assets/DummySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummySound : MonoBehaviour {
+
+
+	void Awake()
+	{
+		Destroy(gameObject,4.0f);
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
