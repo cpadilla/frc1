91f1be00078d06f7fc7fdc8961301f048844c326
Christofer Padilla <christoferpadilla@gmail.com>
2014-07-20 08:45:09 -0400

    committing changes to Menu

---
+++ FRC1/Assets/Scripts/BossBodyMotion.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBodyMotion : MonoBehaviour {
+
+        public float magnitude = 1.0f;
+        public float frequency = .5f;
+        public float angle = 30.0f;
+
+        float xdegrees = 0.0f;
+        float ydegrees = 0.0f;
+        float zdegrees = 0.0f;
+
+        static float rndseed;
+
+	// Use this for initialization
+	void Start () {
+            //rndseed = Time.time + Random()
+            System.Random rnd = new System.Random();
+	
+            float x = Mathf.Cos(rnd.Next() * frequency) / angle;
+            float y = Mathf.Cos(rnd.Next() * frequency) / angle;
+            float z = Mathf.Cos(rnd.Next() * frequency) / angle;
+            transform.Rotate(x, y, z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            //float x = Time.time * xdegrees;
+            //x = Mathf.Cos(x) * angle;
+            float x = Mathf.Cos(Time.time * frequency) / angle;
+            float y = Mathf.Cos(Time.time * frequency) / angle;
+            float z = Mathf.Cos(Time.time * frequency) / angle;
+            transform.Rotate(x, y, z);
+	}
+}
