cf9315ff1199d2dc2df9ac650cb958868e9d3683
John Li <johnli@johns-mbp.fullsail.com>
2014-07-20 17:34:59 -0400

    Added weapon power up drops on Enemy Destroy Player detects and collects power ups.

--- FRC1/Assets/Scripts/Powerup.cs
+++ FRC1/Assets/Scripts/Powerup.cs
@@ -4,13 +4,44 @@ using System.Collections;
 public class Powerup : MonoBehaviour {
 
 	public const int TYPE_LASER = 0;
-	public const int TYPE_BURST = 1;
-	public const int TYPE_RAPID = 2;
+	public const int TYPE_BULLET = 1;
+	public const int TYPE_ARMOR = 2;
+	public const int TYPE_ARMORTWO = 3;
 
 	public int type = 0;
 
-	void Init(int Type) 
+	static Transform player;
+	void Awake()
 	{
-		type = Type;
+		if (player == null)
+			player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		StartCoroutine (CR_DistanceCheck ());
+		transform.parent = GameObject.Find ("WorldRenderer").transform;
+	}
+	
+	float GetDistance()
+	{
+		float val = Mathf.Abs (Vector3.Distance (transform.position, player.position)); 
+		return val;
+	}
+
+	IEnumerator CR_DistanceCheck()
+	{
+		while (true) {
+			if(GetDistance() > 150)
+				Destroy(gameObject);
+			yield return new WaitForSeconds(1); //check every second	
+		}
+	}
+
+	public void OnCollected()
+	{
+		Destroy (gameObject);
+	}
+
+	void OnDestroy()
+	{
+		StopAllCoroutines ();
 	}
 }
